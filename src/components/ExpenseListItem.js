00f08862919d10c8cf5136de5f3b4e20bb7996a1
Siddharth Patel <siddharth_patel@persistent.com>
2021-04-15 15:40:07 +0530

    Setup formatting for amount and created at

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
+// // load a locale
+// numeral.register('locale', 'in', {
+//     currency: {
+//         symbol: '₹'
+//     }
+// });
+
+// // switch between locales
+// numeral.locale('in');
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 )
 
